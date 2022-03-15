d44aacd1f811dd67fac159e086fb300f8218ad1e
AngeloLista <abstractplayer@gmail.com>
2022-03-15 16:03:37 +0100

    added footer links and changed font

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -12,25 +12,100 @@
                   <a href="#">Characters</a>
                 </li>
                 <li>
-                  <a href="#">Characters</a>
+                  <a href="#">Comics</a>
                 </li>
                 <li>
-                  <a href="#">Characters</a>
+                  <a href="#">Movies</a>
                 </li>
                 <li>
-                  <a href="#">Characters</a>
+                  <a href="#">TV</a>
                 </li>
                 <li>
-                  <a href="#">Characters</a>
+                  <a href="#">Games</a>
                 </li>
                 <li>
-                  <a href="#">Characters</a>
+                  <a href="#">Videos</a>
                 </li>
                 <li>
-                  <a href="#">Characters</a>
+                  <a href="#">News</a>
                 </li>
               </ul>
             </section>
+            <section>
+                <!-- Titolo -->
+                <h3>SHOP</h3>
+                <!-- Links -->
+                <ul>
+                  <li>
+                    <a href="#">Shop DC</a>
+                  </li>
+                  <li>
+                    <a href="#">Shop DC Collectibles</a>
+                  </li>
+                </ul>
+              </section>
+              <section>
+                <!-- Titolo -->
+                <h3>DC</h3>
+                <!-- Links -->
+                <ul>
+                  <li>
+                    <a href="#">Term of Use</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy policy (New)</a>
+                  </li>
+                  <li>
+                    <a href="#">Ad Choices</a>
+                  </li>
+                  <li>
+                    <a href="#">Advertising</a>
+                  </li>
+                  <li>
+                    <a href="#">Jobs</a>
+                  </li>
+                  <li>
+                    <a href="#">Subscriptions</a>
+                  </li>
+                  <li>
+                    <a href="#">Talent Workshop</a>
+                  </li>
+                  <li>
+                    <a href="#">CPSC Certificates</a>
+                  </li>
+                  <li>
+                    <a href="#">Ratings</a>
+                  </li>
+                  <li>
+                    <a href="#">Shop Help</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact Us</a>
+                  </li>
+                </ul>
+              </section>
+              <section>
+                <!-- Titolo -->
+                <h3>SITES</h3>
+                <!-- Links -->
+                <ul>
+                  <li>
+                    <a href="#">DC</a>
+                  </li>
+                  <li>
+                    <a href="#">MAD Magazine</a>
+                  </li>
+                  <li>
+                    <a href="#">DC Kids</a>
+                  </li>
+                  <li>
+                    <a href="#">DC Universe</a>
+                  </li>
+                  <li>
+                    <a href="#">DC Power Visa</a>
+                  </li>
+                </ul>
+              </section>
           </nav>
           <div class="dc-logo-bg"></div>
         </div>
