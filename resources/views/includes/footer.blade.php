737fe60d214a143ac1837b0315708e3f1d294f4c
AngeloLista <abstractplayer@gmail.com>
2022-03-15 15:52:57 +0100

    added footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,85 @@
+<footer>
+    {{-- Footer Top --}}
+    <section id="footer-top">
+        <div class="d-flex justyify-space-between container">
+          <nav>
+            <section>
+              <!-- Titolo -->
+              <h3>DC Comics</h3>
+              <!-- Links -->
+              <ul>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+              </ul>
+            </section>
+          </nav>
+          <div class="dc-logo-bg"></div>
+        </div>
+      </section>
+
+      {{-- Footer Bottom --}}
+      <div id="footer-bottom">
+        <div class="container d-flex align-items-center justyify-space-between">
+          <div id="sign-up"><a href="#">SIGN-UP NOW!</a></div>
+          <div id="socials">
+            <h3>FOLLOW US</h3>
+            <ul>
+              <li>
+                <a href="#"
+                  ><img
+                    src="{{ asset('images/footer-facebook.png') }}"
+                    alt="Facebook Logo"
+                /></a>
+              </li>
+              <li>
+                <a href="#"
+                  ><img
+                    src="{{ asset('images/footer-twitter.png') }}"
+                    alt="Twitter Logo"
+                /></a>
+              </li>
+              <li>
+                <a href="#"
+                  ><img
+                    src="{{ asset('images/footer-youtube.png') }}"
+                    alt="YouTube Logo"
+                /></a>
+              </li>
+              <li>
+                <a href="#"
+                  ><img
+                    src="{{ asset('images/footer-pinterest.png') }}"
+                    alt="Pinterest Logo"
+                /></a>
+              </li>
+              <li>
+                <a href="#"
+                  ><img
+                    src="{{ asset('images/footer-twitter.png') }}"
+                    alt="Periscope Logo"
+                /></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,13 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }} | @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}">
 </head>
 <body>
     @include('includes.header')
-
+    
     <main>
-        adsfadfs
+        CONTENUTO
     </main>
     
+    
+    @include('includes.footer')
+    
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
