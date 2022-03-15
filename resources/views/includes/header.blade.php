d8e4801ad5a92bb8a70a3e1f27c2e0914e21260f
AngeloLista <abstractplayer@gmail.com>
2022-03-15 15:32:05 +0100

    added header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,40 @@
+<header>
+    <div class="container d-flex-j-between h-130">
+      <a href="#"><img src="{{ asset('images/dc-logo.png') }}" alt="Logo DC" /></a>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">CHARACTERS</a>
+          </li>
+          <li>
+            <a href="#">COMICS</a>
+          </li>
+          <li>
+            <a href="#">MOVIES</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">GAMES</a>
+          </li>
+          <li>
+            <a href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a href="#">FANS</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
