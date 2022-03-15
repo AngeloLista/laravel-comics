d84a50701c823052923de36c128911747da1d17c
AngeloLista <abstractplayer@gmail.com>
2022-03-15 16:30:11 +0100

    added dynamic header menu items through header_menu config file

---
+++ config/header_menu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    ["text" => "CHARACTERS", "route_name" => "characters"],
+    ["text" => "COMICS", "route_name" => "comics"],
+    ["text" => "MOVIES", "route_name" => "movies"],
+    ["text" => "TV", "route_name" => "comics"],
+    ["text" => "GAMES", "route_name" => "comics"],
+    ["text" => "COLLECTIBLES", "route_name" => "comics"],
+    ["text" => "VIDEOS", "route_name" => "comics"],
+    ["text" => "FANS", "route_name" => "comics"],
+    ["text" => "NEWS", "route_name" => "comics"],
+    ["text" => "SHOP", "route_name" => "comics"],
+];
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,39 +1,19 @@
+@php
+    $links = config('header_menu')
+@endphp
+
+
 <header>
     <div class="container d-flex-j-between h-130">
       <a href="#"><img src="{{ asset('images/dc-logo.png') }}" alt="Logo DC" /></a>
 
       <nav>
         <ul>
-          <li>
-            <a href="{{ route('characters') }}">CHARACTERS</a>
-          </li>
-          <li>
-            <a href="{{ route('comics') }}">COMICS</a>
-          </li>
-          <li>
-            <a href="{{ route('movies') }}">MOVIES</a>
-          </li>
-          <li>
-            <a href="#">TV</a>
-          </li>
-          <li>
-            <a href="#">GAMES</a>
-          </li>
-          <li>
-            <a href="#">COLLECTIBLES</a>
-          </li>
-          <li>
-            <a href="#">VIDEOS</a>
-          </li>
-          <li>
-            <a href="#">FANS</a>
-          </li>
-          <li>
-            <a href="#">NEWS</a>
-          </li>
-          <li>
-            <a href="#">SHOP</a>
-          </li>
+          @foreach ($links as $link)
+            <li>
+            <a href="{{ route($link['route_name']) }}">{{ $link['text'] }}</a>
+            </li>  
+          @endforeach
         </ul>
       </nav>
     </div>
